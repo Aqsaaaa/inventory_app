77ceb9a5d650652e160a06ffa1cc26f205b6b3ea
rey <reynaldigazali@gmai.com>
2025-02-23 07:22:11 +0800

    Add login desain

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart'
-  show FlutterSecureStorage;
+    show FlutterSecureStorage;
 import 'package:google_fonts/google_fonts.dart';
 import 'package:inventory/screens/bottom_bar_screen.dart';
 import 'package:inventory/screens/stats_screen.dart';
@@ -17,33 +17,33 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   Future<bool> checkToken() async {
-  final storage = FlutterSecureStorage();
-  final token = await storage.read(key: 'token');
-  return token != null;
+    final storage = FlutterSecureStorage();
+    final token = await storage.read(key: 'token');
+    return token != null;
   }
 
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Inventory Management',
-    theme: ThemeData(
-    textTheme: GoogleFonts.poppinsTextTheme(),
-    visualDensity: VisualDensity.adaptivePlatformDensity,
-    ),
-    home: FutureBuilder<bool>(
-    future: checkToken(),
-    builder: (context, snapshot) {
-      if (snapshot.connectionState == ConnectionState.waiting) {
-      return const CircularProgressIndicator();
-      }
-      return snapshot.data == false ? BottomNavBar() : LoginScreen();
-    },
-    ),
-    routes: {
-    '/dashboard': (context) => BottomNavBar(),
-    '/add_item': (context) => AddItemScreen(),
-    '/stats_status': (context) => ItemStatsScreen(),
-    },
-  );
+    return MaterialApp(
+      title: 'Inventory Management',
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FutureBuilder<bool>(
+        future: checkToken(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          return snapshot.data == false ? BottomNavBar() : LoginScreen();
+        },
+      ),
+      routes: {
+        '/dashboard': (context) => BottomNavBar(),
+        '/add_item': (context) => AddItemScreen(),
+        '/stats_status': (context) => ItemStatsScreen(),
+      },
+    );
   }
 }
